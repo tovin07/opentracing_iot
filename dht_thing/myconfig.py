5c38bb3bd43d3e281068b01047a025d002e0893a
duyleomessi <duyleitbka95@gmail.com>
2018-04-21 00:56:49 +0700

    add config

---
+++ dht_thing/myconfig.py
@@ -0,0 +1,15 @@
+class ZipkinConfig:
+    host = '10.42.0.1'
+    # host = '192.168.43.127'
+    port = 9411
+    
+
+class MqttConfig:
+    # broker_address='m12.cloudmqtt.com'
+    # port=14513
+    clientName='duyld_openhab2'
+    # user='xrzpsblg'
+    # passworld='o1uqAZ62fbpe'
+    broker_address='10.42.0.1'
+    port='1883'
+
